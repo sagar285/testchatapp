8882ea4487c4ead15537ecb38143b9e302935698
sagar <96648429+sagar285@users.noreply.github.com>
2024-04-04 12:46:17 +0530

    Update index.js

--- backendchatapp/index.js
+++ backendchatapp/index.js
@@ -1,9 +1,14 @@
 const express =require("express")
+const { Server } = require("socket.io");
 require("./conn/connection")
 const userRoute  = require("./routes/user")
 const chatRoute  = require("./routes/chats")
 const messageRoute  = require("./routes/messages")
+const http = require('http')
+const chatSocket = require("./socket");
+
 const app =express()
+const server = http.createServer(app) 
 const dotenv = require("dotenv")
 dotenv.config()
 app.use(express.json())
@@ -11,10 +16,16 @@ app.use(userRoute);
 app.use(chatRoute);
 app.use(messageRoute);
 
+const io = new Server(server, {
+    cors: {
+      origin: "*"
+    }
+  })
+
+  chatSocket(io)
 
 
 
-
-app.listen(3000,()=>{
-    console.log(`server listening on port no 3000`)
-})
+  server.listen(3000, () => {
+    console.log(`Server is listening at http://localhost:3000`);
+  });
